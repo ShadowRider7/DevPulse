6d05091613cf0b866149c1cecd8bbd916bc410b0
MD. SAFAYET HOSSAIN SADHIN <sadhinv@gmail.com>
2026-05-22 11:41:31 +0600

    server separated from app

---
+++ src/app/config/index.ts
@@ -0,0 +1,34 @@
+import { pool } from "./db";
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(20) DEFAULT 'contributor',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS issues(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+        type VARCHAR(30) NOT NULL,
+        status VARCHAR(30) DEFAULT 'open',
+        reporter_id INTEGER NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    console.log("Database initialized");
+  } catch (error) {
+    console.log(error);
+  }
+};
